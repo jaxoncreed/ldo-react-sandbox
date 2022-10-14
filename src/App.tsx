59476e522d98270476adb77cbf1630ac59eabc6b
jaxoncreed <jaxoncreed@gmail.com>
2022-10-14 11:15:11 -0400

    init problem

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,31 @@ import React, { useEffect, useState } from 'react';
 import { FoafProfileFactory } from './ldo/foafProfile.ldoFactory';
 import { FoafProfile } from './ldo/foafProfile.typings';
 
+import { construct } from "@shexjs/validator";
+// The typings for neighborhood-rdfjs are incorrect
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { ctor } from "@shexjs/neighborhood-rdfjs";
+import { FoafProfileShapeType } from './ldo/foafProfile.shapeTypes';
+
 function App() {
   const [profile, setProfile] = useState<FoafProfile>();
 
   useEffect(() => {
-    const profile = FoafProfileFactory.new();
+    const profile = FoafProfileFactory.new("https://example.com/profile");
     profile.name = "Jackson Morgan";
+    profile.type = { "@id": "Person" };
+
+    const profileId = profile["@id"] || "";
+
+    // Validate
+    const validationResult = construct(FoafProfileShapeType.schema, ctor(profile.$dataset()), {}).validate([
+      {
+        node: profileId,
+        shape: FoafProfileShapeType.shape,
+      },
+    ]);
+    console.log(JSON.stringify(validationResult, null, 2));
     setProfile(profile);
   }, []);
 
